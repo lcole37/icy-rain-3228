8879fb91b0b07650119ff0d0c806588286839d3f
Lucas Colwell <colwellc37@gmail.com>
2022-08-09 09:26:30 -0600

    Test: fix typos and spacing

--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Flight, type: :model do
-  it {should belong_to :airline}
-  it {should have_many :passenger_flights}
-  it {should have_many :airlines, through: :passenger_flights}
+  it { should belong_to :airline }
+  it { should have_many :passenger_flights }
+  it { should have_many :passengers, through: :passenger_flights }
 end
