f58eb305f5c43b71de1232fec5615ebb598bfdd8
Lucas Colwell <colwellc37@gmail.com>
2022-08-09 11:14:09 -0600

    Add method to controller

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,4 +2,9 @@ class FlightsController < ApplicationController
   def index
     @flights = Flight.all
   end
+
+  def delete #should this be in passengers controller file?
+    Passenger.delete(params[:id])
+    redirect_to '/flights'
+  end
 end
