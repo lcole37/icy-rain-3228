4e9402a47d3380a6df54604505fa1548bf632447
Lucas Colwell <colwellc37@gmail.com>
2022-08-09 09:59:51 -0600

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+united = Airline.create!(name: "United")
+southwest = Airline.create!(name: "Southwest")
+delta = Airline.create!(name: "Delta")
+
+flight1 = united.flights.create!(number: 1, date: "August 4", departure_city: "Austin", arrival_city: "LA")
+flight2 = southwest.flights.create!(number: 1, date: "January 6", departure_city: "Dublin", arrival_city: "Minneapolis")
+flight3 = delta.flights.create!)(number: 1, date: "May 9", departure_city: "Atlanta", arrival_city: "Lincoln")
+
+larry = Passengers.create!(name: "Larry", age: 20)
+mary = Passengers.create!(name: "Mary", age: 30)
+gary = Passengers.create!(name: "Gary", age: 40)
+sherri = Passengers.create!(name: "Sherri", age: 50)
