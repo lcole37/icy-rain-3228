d966e3f6dc3a66d860a64757e238f773969c6df7
Lucas Colwell <colwellc37@gmail.com>
2022-08-09 11:25:52 -0600

    Add route for story 3

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/flights', to: 'flights#index'
   delete '/flights/:id', to: 'flights#destroy' #unsure if this should be passengers or flights
+  get '/airlines/:id', to: 'airlines#show'
 end
