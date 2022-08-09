04f796acd764dc5a68454b65203e5079cabcad44
Lucas Colwell <colwellc37@gmail.com>
2022-08-09 11:57:24 -0600

    Story 3 attempt

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -2,7 +2,7 @@ class Airline < ApplicationRecord
   has_many :flights
 
   def adult_passengers
-    passengers
-   #need a lot of work figuring this out
+    #this method needs to find all/select passengers where age >= 18.
+   #need a lot of work figuring this out.
   end
 end
