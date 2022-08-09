283febe4d206185cb7037038d6a1c693b5d36920
Lucas Colwell <colwellc37@gmail.com>
2022-08-09 09:13:01 -0600

    Test: model test flight

--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -2,4 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Flight, type: :model do
   it {should belong_to :airline}
+  it {should have_many :passenger_flights}
+  it {should have_many :airlines, through: :passenger_flights}
 end
